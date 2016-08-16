facc39bab5e9a41b71acd9f3117c45051940f07d
Prayag Barua <pbarua@pobox.com>
2016-08-16 20:54:18 +0200

    version1

--- bberg.js
+++ bberg.js
@@ -1,55 +1,81 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs');
 
 // Find a better way to build this array
 alphabets = ["A","B"];
 var index = 0;
+var dataIndex = 0;
 var queue = [];
+var obj = {};
 var firstRow = 0;
+var nextPageUrl = ' ';
 
 
 getCompanyData();
-var index = 0;
-getData();
 
-
-function getCompanyData(alphabets[index]) {
+// fs.appenbdFIld('bla', JSON.stringify(obje)
+function getCompanyData() {
+  if (firstRow == 0) {
     var url = 'http://www.bloomberg.com/Research/common/symbollookup/symbollookup.asp?letterIn=' + alphabets[index];
-    request(url, function(err, resp, body) {
-        if (err)
-            throw err;
-        $ = cheerio.load(body);
-        $('.table tbody tr td a').each(function() {
+  } else {
+    var url = 'http://www.bloomberg.com/Research/common/symbollookup/' + nextPageURL;
+  };
+  request(url, function(err, resp, body) {
+      if (err)
+          throw err;
+      $ = cheerio.load(body);
+      $('.table tbody tr td a').each(function() {
           var stockPage = $(this).attr('href').replace('../../','/');
           var newURL = 'http://www.bloomberg.com/Research' + stockPage;
           queue.push(newURL);
           console.log('newURL ' + newURL);
-        });
+      });
+
 // next page
-//
-//
-//
-        index++;
-        if (index < alphabets.length) {
-          getCompanyData(alphabets[index]);
-        };
-    });
+      $('.nextBtnActive').each(function () {
+          nextPageURL = $(this).attr('href');
+      });
+      var last7 = nextPageURL.substr(nextPageURL.length - 7)
+      var firstRowNextPage = last7.split("=").pop();
+
+      if (firstRowNextPage > firstRow) {
+          firstRow = firstRowNextPage;
+          getCompanyData();
+      } else {
+
+// Get individual stock data
+          dataIndex = 0;
+          getData();
+      };
+  });
 }
 
 function getData() {
-    var url = queue[index];
+    var url = queue[dataIndex];
     if (url !== ' ') {
         request(url, function(err, resp, body) {
             if (err)
             throw err;
             $ = cheerio.load(body);
-            console.log($('h1.name').text());
-            console.log($('.ticker').text());
-            console.log($('.exchange').text());
+            obj.name = ($('h1.name').text()).replace(/\\n/g,'').trim();
+            obj.ticker = ($('.ticker').text()).replace(/\\n/g,'').trim();
+            obj.exchange = ($('.exchange').text()).replace(/\\n/g,'').trim();
+            fs.appendFile('bloomberg.json', JSON.stringify(obj) + ',\n', function(err) {
+              if (err)
+                throw err;
+            });
+            dataIndex ++;
+            if (dataIndex < queue.length) {
+                getData();
+            } else {
+                queue = [];
+                firstPage = 0;
+                index++;
+                if (index < alphabets.length) {
+                    getCompanyData();
+                };
+            };
         });
-    };
-    index ++;
-    if (index < queue.length) {
-      getData();
     };
 };
